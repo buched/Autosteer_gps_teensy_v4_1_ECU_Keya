5009aeea17afa0e5cb25e1db17b50e6ad75f50ea
buched <32975584+buched@users.noreply.github.com>
2025-11-06 19:28:49 +0100

    Update KeyaCANBUS.ino

--- Autosteer_gps_teensy_v4_1_ECU_Keya_V201_all_brands/KeyaCANBUS.ino
+++ Autosteer_gps_teensy_v4_1_ECU_Keya_V201_all_brands/KeyaCANBUS.ino
@@ -209,15 +209,13 @@ void KeyaBus_Receive()
                   eng();
                 }
             }
-           if (aogConfig.isRelayActiveHigh == 1)
-                {
-                  if (KBusReceiveData.id == 0x18FE4523)
+           
+            if (KBusReceiveData.id == 0x18FE4523)
                     {
                       KBUSRearHitch = (KBusReceiveData.buf[0]);
                       if (KBUSRearHitch < aogConfig.user1) workCAN = 1;
                       else workCAN = 0;
-                    }   
-                }
+                    } 
         }
       if (Brand == 3)
         {
@@ -276,3 +274,4 @@ void eng()
                             }
                       }
 }
+
